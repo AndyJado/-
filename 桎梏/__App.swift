5f2dc4ba36826e7bca7c8a9151c6f9fe2e6f03d7
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-06-23 10:04:59 +0800

    Initial Commit

---
+++ 桎梏/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  桎梏
+//
+//  Created by Andy Jado on 2022/6/23.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
